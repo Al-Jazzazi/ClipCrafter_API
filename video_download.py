74e5f6798eb3172cd1b636655e4e9e940b05ad7d
Al-Jazzazi <ya2225@nyu.edu>
2024-06-30 19:07:04 -0400

    getting the video downloader working

--- video_download.py
+++ video_download.py
@@ -1,6 +1,14 @@
 import yt_dlp
 
 def download_video(url, output_name):
-    ydl_opts = {'outtmpl': output_name}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    yt_opts = {
+    'verbose': True,
+    
+    } 
+    ydl = yt_dlp.YoutubeDL(yt_opts)
+    ydl.download(url)
+
+url = "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+download_video(url, "first video on yt" )
+
+
