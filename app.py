ebd99bbc5a33c9c354ee3a4747a68aff29ccae3d
Al-Jazzazi <ya2225@nyu.edu>
2024-07-01 21:56:23 -0400

    development in the backend and connecting app.py to the video_download.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash 
 from datetime import timedelta 
+# import json 
+from video_download import download_video
 
 app = Flask(__name__)
 app.secret_key = "Me Yousef "
@@ -7,22 +9,35 @@ app.permanent_session_lifetime = timedelta(minutes= 2)
 
 @app.route('/')
 def hello():
-    return "Video Processor API"
+    return render_template("index.html")
 
-
-@app.route('/download_video', methods=['POST'])
-def download_video_endpoint():
-    data = request.json
-    url = data.get('url')
-    if not url:
-        return jsonify({"error": "URL is required"}), 400
+@app.route('/download', methods = ["POST", "GET"])
+def download(): 
+    if request.method == "POST": 
+        url = request.form["url"]
+        return redirect(url_for("video", url = url))
+    else:
+        return render_template("download.html")
     
-    output_path = 'videos/%(title)s-%(id)s.%(ext)s'
-    try:
-        download_video(url, output_path)
-        return jsonify({"video_url": output_path})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+
+@app.route('/video/<url>')
+def video(url): 
+    download_video(url)
+    return render_template("video_link.html", url = url)
+
+# @app.route('/download_video', methods=['POST'])
+# def download_video_endpoint():
+#     data = request.json
+#     url = data.get('url')
+#     if not url:
+#         return jsonify({"error": "URL is required"}), 400
+    
+#     output_path = 'videos/%(title)s-%(id)s.%(ext)s'
+#     try:
+#         download_video(url, output_path)
+#         return jsonify({"video_url": output_path})
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
--- video_download.py
+++ video_download.py
@@ -1,14 +1,15 @@
 import yt_dlp
 
-def download_video(url, output_name):
+def download_video(url):
+    full_url = " https://www.youtube.com/" + url
     yt_opts = {
     'verbose': True,
     
     } 
     ydl = yt_dlp.YoutubeDL(yt_opts)
-    ydl.download(url)
+    ydl.download(full_url)
 
-url = "https://www.youtube.com/watch?v=jNQXAC9IVRw"
-download_video(url, "first video on yt" )
+# url = "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+# download_video(url, "first video on yt" )
 
 
